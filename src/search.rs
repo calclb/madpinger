48d067030ce66bb4d6573f03c7f4c691820ab8f3
calculub <42684303+calculub@users.noreply.github.com>
2023-04-21 17:26:07 -0500

    pass tests for section subcommand; search subcommmand now reports basic course info; make major schema refactor

--- src/search.rs
+++ src/search.rs
@@ -12,7 +12,9 @@ use reqwest::header::{
 };
 use reqwest::Client;
 use reqwest::Error as ReqwestError;
+use serde::Serialize;
 
+use crate::search::schema::{SearchApiPing, SearchedCourse};
 use crate::CourseStatusFilters;
 
 const SEARCH_POST_URI_BASE: &str = "https://public.enroll.wisc.edu/api/search/v1";
@@ -70,10 +72,10 @@ pub async fn get_search_info(
     search: &str,
     size: usize,
     filters: CourseStatusFilters,
-) -> Result<(), ReqwestError> {
+) -> Result<SearchApiPing, ReqwestError> {
     let payload = get_payload(term_code, search, size, filters);
 
-    println!("Request body len: {}", payload.len());
+    // println!("Request body len: {}", payload.len());
     let mut hdrs = HeaderMap::new();
     hdrs.insert(ACCEPT, HeaderValue::from_static("application/json")); // not required
     hdrs.insert(
@@ -94,12 +96,110 @@ pub async fn get_search_info(
 
     println!("Response status: {:#?}", resp.status());
 
-    let text = resp.text().await?;
-    println!("Response len: {:#?}", text.len());
+    let searched_courses = resp.json::<SearchApiPing>().await?;
 
     File::create(OUTPUT_FILE_NAME)
         .unwrap()
-        .write_all(text.as_bytes())
-        .unwrap();
-    Ok(())
+        .write_all(
+            serde_json::to_string(&searched_courses)
+                .expect("couldn't convert to string")
+                .as_bytes(),
+        )
+        .expect("failed to write");
+
+    Ok(searched_courses)
+}
+
+pub mod schema {
+    use crate::section;
+    use serde::{Deserialize, Serialize};
+
+    #[derive(Debug, Serialize, Deserialize)]
+    #[serde(rename_all = "camelCase")]
+    pub struct SearchApiPing {
+        pub found: usize,
+        pub hits: Vec<SearchedCourse>,
+    }
+
+    #[derive(Debug, Serialize, Deserialize)]
+    #[serde(rename_all = "camelCase")]
+    pub struct SearchedCourse {
+        pub term_code: String,
+        pub course_id: String,
+        pub subject: section::schema::Subject,
+        pub catalog_number: String,
+        pub approved_for_topics: bool,
+        pub topics: Vec<Topic>,
+        pub minimum_credits: usize,
+        pub maximum_credits: usize,
+        pub credit_range: String,
+        pub first_taught: Option<String>,
+        pub last_taught: Option<String>,
+        pub typically_offered: String,
+        pub general_ed: Option<ReqGoalAbbrev>,
+        pub ethnic_studies: Option<ReqGoalAbbrev>,
+        pub breadths: Vec<ReqGoalAbbrev>,
+        pub letters_and_science_credits: Option<ReqGoalAbbrev>,
+        pub workplace_experience: Option<ReqGoalAbbrev>,
+        pub foreign_language: Option<ReqGoalAbbrev>,
+        // honors: Option<?>,
+        pub levels: Vec<ReqGoalAbbrev>,
+        pub open_to_first_year: bool,
+        // advisory_prerequisites: Option<AdvisoryPrerequisite>,
+        pub enrollment_prerequisites: Option<String>,
+        // all_cross_listed_subjets: Vec<?>,
+        pub title: String,
+        pub description: String,
+        pub catalog_print_flag: bool,
+        // academic_group_code: Vec<AcademicGroupCode>,
+        pub currently_taught: bool,
+        // grading_basis: GradingBasis,
+        pub repeatable: String,
+        // grad_course_work: Option<?>,
+        // sustainability: Option<?>,
+        // instructor_provided_content: Option<?>,
+        // course_requirements: CourseRequirements,
+        pub course_designation: String,
+        pub course_designation_raw: String,
+        pub full_course_designation: String,
+        pub full_course_designation_raw: String,
+        pub last_updated: u64,
+        pub catalog_sort: String,
+        pub subject_aggregate: String,
+        pub title_suggest: TitleSuggestion,
+        #[serde(rename = "matched_queries")]
+        pub matched_queries: Option<Vec<String>>,
+    }
+
+    #[derive(Debug, Serialize, Deserialize)]
+    #[serde(rename_all = "camelCase")]
+    pub struct Topic {
+        pub short_description: String,
+        pub long_description: String,
+        pub id: usize,
+        pub topic_last_taught: String, // gives a term code
+    }
+
+    /// Links a title (seen in `input`) with the course id.
+    #[derive(Debug, Serialize, Deserialize)]
+    #[serde(rename_all = "camelCase")]
+    pub struct TitleSuggestion {
+        pub input: Vec<String>,
+        pub payload: CourseIdObj,
+    }
+
+    #[derive(Debug, Serialize, Deserialize)]
+    #[serde(rename_all = "camelCase")]
+    pub struct CourseIdObj {
+        pub course_id: String,
+    }
+
+    /// A pair
+    /// This type of information seems similar to that displayed by DARS audits.
+    #[derive(Debug, Serialize, Deserialize)]
+    #[serde(rename_all = "camelCase")]
+    pub struct ReqGoalAbbrev {
+        pub code: String,
+        pub description: String,
+    }
 }
--- tests/section_get.rs
+++ tests/section_get.rs
@@ -3,39 +3,58 @@ use std::fs::File;
 use std::io::{BufRead, BufReader};
 use std::time::Duration;
 
+use madpinger::{default_client_headers, API_SRC_FILE};
+use reqwest::Client;
 use tokio::time::sleep;
 
 use madpinger::section::{get_section_info, SECTION_GET_URI_BASE};
 
-const BATCH_PAUSE: Duration = Duration::from_secs(5);
-const BATCH_REQUEST_SIZE: usize = 20;
+const BATCH_PAUSE: Duration = Duration::from_secs(10);
+const BATCH_REQUEST_SIZE: usize = 100;
+const SKIP_LINES: usize = 5000; // don't want the test to go through the entire API every commit
 
 /// Tests that the deserialization process does not throw an error when parsing the API responses from the UW-Madison CS&E site.
 /// If an error does occur, it's likely because the schema is still misconfigured.
 ///
-/// Internally, the test reads from `course_sections.txt` to call [`section::get_section_info`]().
+/// Internally, the test reads from `course_sections.csv` to call [`section::get_section_info`]().
 ///
 /// Due to rate-limiting concerns, this function will sometimes run an asynchronous pause.
 #[tokio::test]
-async fn no_errors_exhaustive() -> Result<(), Box<dyn Error>> {
-    let f = File::open("./course_sections.txt").expect("could not find `course_section.txt`");
+async fn no_deser_errors_exhaustive() -> Result<(), Box<dyn Error>> {
+    let f = File::open(API_SRC_FILE)
+        .expect("couldn't open the API file to load necessary request info");
     let br = BufReader::new(f);
+    let client = Client::builder()
+        .default_headers(default_client_headers())
+        .cookie_store(true)
+        .connect_timeout(Duration::from_secs(10))
+        .timeout(Duration::from_secs(10))
+        .build()?;
 
-    for (i, l) in br.lines().skip(1).enumerate() {
+    for (i, l) in br.lines().skip(1).enumerate().skip(SKIP_LINES) {
         // ignore header line
         if let Ok(line) = l {
-            let v: Vec<&str> = line.splitn(3, "/").collect();
+            let v: Vec<&str> = line.splitn(3, ',').collect();
+            let tc = v[0];
+            let sc = v[1];
+            let cid = v[2];
+
             println!(
-                "hit {}: {}/{}",
+                "hit {}: {}/{}/{}/{}",
                 i + 1,
                 SECTION_GET_URI_BASE,
-                line.to_string()
+                tc,
+                sc,
+                cid,
             );
-            let _ = get_section_info(v[0], v[1], v[2]).await?;
+            let _ = get_section_info(&client, tc, sc, cid).await?;
+        } else {
+            println!("(skipped line {}; was malformed)", i);
         }
 
         if i != 0 && i % BATCH_REQUEST_SIZE == 0 {
             // avoid rate-limiting (or ip blacklist)
+            println!("sleeping for {}s", BATCH_PAUSE.as_secs_f64());
             sleep(BATCH_PAUSE).await;
         }
     }
